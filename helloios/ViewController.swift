c24d310d228c3fe45021b74aa4a645c33e83a545
Steven <linguinealfredo@gmail.com>
2017-12-16 15:55:04 -0800

    made button that changes text

--- helloios/ViewController.swift
+++ helloios/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        messageLabel.text = "something different"
+    }
+    
 }
 
